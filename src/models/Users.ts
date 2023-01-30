d1eb5195ecfb1a9365fce6c50229ec6833403abf
Thiago Almeida <tcalmeida85@gmail.com>
2023-01-30 18:58:18 -0300

    feat: create user model

---
+++ src/models/Users.ts
@@ -0,0 +1,38 @@
+import { DataTypes } from "sequelize";
+import { mySqlConection } from "../database";
+
+const dbConnection = mySqlConection.getInstance();
+
+const User = dbConnection.define(
+  "Users",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    telephone: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    tableName: "users",
+  }
+);
+
+export default User;
