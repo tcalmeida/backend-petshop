f742b9c027986d690f1ce98a29cc1b0261270dce
tiagospeckart <tiagospeckart@gmail.com>
2023-01-31 22:03:26 -0300

    build: routes index file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { mySqlConection } from "./database";
+import { routes } from "./routes";
 import "dotenv/config";
+
 
 const APP_PORT:number = 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 mySqlConection.hasConection();
 
